a9e1fb2698ace0dc726cb1a94a789ec81ea39bc0
cooper0G9 <karamzinaulia97@gmail.com>
2022-02-19 19:53:25 +0300

    Initial commit

---
+++ src/HomeWorkApp.java
@@ -0,0 +1,59 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a=8;
+        int b=-9;
+        int sum=a+b;
+        if (sum>=0) {
+            System.out.println("Сумма положительная");
+        }
+        if (sum<0) {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value=111;
+        if (value<=0){
+            System.out.println("Красный");
+        }
+        if (value>0 && value<=100){
+            System.out.println("Желтый");
+        }
+        if (value>100){
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 3;
+        int b = 9;
+        if (a>=b) {
+            System.out.println("a>=b");
+        }
+        if (a<b) {
+            System.out.println("a<b");
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
