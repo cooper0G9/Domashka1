1a4ce52fcf69244800dee184c96757603210baea
cooper0G9 <karamzinaulia97@gmail.com>
2022-03-29 20:33:38 +0300

    Les7

---
+++ src/Lesson7/Plate.java
@@ -0,0 +1,18 @@
+package Lesson7;
+
+public class Plate {
+    private int food;
+
+
+    Plate (int food){
+        this.food = food;
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public void setFood(int food) {
+        this.food = food;
+    }
+}
